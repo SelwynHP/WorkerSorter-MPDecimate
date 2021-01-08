df56cff8706f11cd5f78b106502c7baad778401b
Selwyn Horne-Primus <Selwyn_AnthonyHP@live.ca>
2021-01-08 11:57:41 -0500

    Create workersorter-mpdecimate.ps1
    Initial Commit

---
+++ workersorter-mpdecimate.ps1
@@ -0,0 +1,18 @@
+﻿$path = $('.\'+'MPDecimate')
+if($(Test-Path $path) -ne $true){New-Item -Path $path -ItemType Directory}
+else
+{
+    $files = (Get-ChildItem -Path $path)
+    foreach($var in $files)
+    {
+        $ogName = $var.Name
+        $tempName = ($var.BaseName + ".temp" + $var.Extension)
+        $ogName
+        $tempName
+        #Move-Item $ogName $($path+'\'+$tempName)
+        ffmpeg -i $var.FullName -vf mpdecimate -c:v hevc_nvenc -preset 11 -c:a copy -vsync vfr $($path+'\'+$tempName)
+        Remove-Item $var.FullName
+        Rename-Item $($path+'\'+$tempName).Replace("[", "``[").replace("]", "``]") $ogName
+    }
+}
+$files
